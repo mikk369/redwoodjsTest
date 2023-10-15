1010d6781ff34fbe7a74e96d61080980d1687471
mikk369 <mikk.ereline@vikk.ee>
2023-10-15 17:51:21 +0300

    create posts,database migrate, separate components

---
+++ web/src/layouts/GardenLayout/GardenLayout.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import GardenLayout from './GardenLayout'
+
+const meta: Meta<typeof GardenLayout> = {
+  component: GardenLayout,
+}
+
+export default meta
+
+type Story = StoryObj<typeof GardenLayout>
+
+export const Primary: Story = {}
